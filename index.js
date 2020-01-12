bf32f660cb4775a51a63351873d70eff65862918
villadeodara <villadeodara@gmail.com>
2020-01-12 10:20:35 -0800

    mobile nav

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function handleSlides() {
+    $('#menu-button').click(function(e){
+		e.stopPropagation();
+		if($('body').hasClass('opened-menu')) closeMobileNav();
+		else openMobileNav();
+    });
+    
     if($('.slideshow').length>0){
 	homeSlideshow =	$('.slideshow').cycle({
 	    slides: "> .slide",
@@ -20,6 +26,18 @@ function handleSlides() {
 		homeSlideshow.cycle('resume');
 	    });
     }
+}
+
+function openMobileNav(delayDur){
+    $('.mobile-nav').delay(delayDur?delayDur:0).slideDown(500);
+    $('#menu-button').addClass('active');
+    $('body').addClass('opened-menu');
+}
+
+function closeMobileNav(delayDur){
+    $('.mobile-nav').delay(delayDur?delayDur:0).slideUp(200);
+    $('#menu-button').removeClass('active');
+    $('body').removeClass('opened-menu');
 }
 
 $(handleSlides);
